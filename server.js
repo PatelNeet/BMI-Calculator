9f2190c15b83dea76a132caca7997ecd9f1b8a18
Neet Patel <66862999+PatelNeet@users.noreply.github.com>
2021-06-17 14:12:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.listen(3000, function(){
+    console.log("Server has started on port 3000");
+});
+
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/",function(req,res){
-    console.log(req.body);
-    var n1 = Number(req.body.num1);
-    var n2 = Number(req.body.num2);
-    var result = n1+n2;
-    res.send("The answer is "+ result);
+app.post("/bmi",function(req,res){
+    // console.log(req.body);
+    var h = Number(req.body.num1);
+    var w = Number(req.body.num2);
+    var BMI = w/(h*h);
+    res.send("<center><h1>The Body Mass Index is "+ BMI + " kg/m*m </h1></center>");
 });
 
-app.listen(3000, function(){
-    console.log("Server has started on port 3000");
-});
+// app.get("/about",function(req,res){
+//     res.send("<h1>Neet Patel</h1><p>This is preety hard to understand</p>");
+// });
